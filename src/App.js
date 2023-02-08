7a7779c5e8d653a50e5e44f9d7eb1b4d85043011
nevenaprtljaga <nevenaprtljaga@gmail.com>
2023-02-08 16:31:34 +0100

    created expenses components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Expenses from "./components/Expenses";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "toilet paper",
+      amount: "222",
+      date: new Date(2023, 1, 1),
+    },
+    {
+      id: "e2",
+      title: "tv",
+      amount: "5000",
+      date: new Date(2022, 2, 12),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
